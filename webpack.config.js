0680e6ac3fd2a86b3764a1fe711928a8c8a84de4
Peter Myer <peter.sr.myer@gmail.com>
2022-10-27 13:09:02 -0500

    adjusting webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require('webpack');
-require('dotenv').config( {
+const dotenv= require('dotenv').config( {
   path: path.join(__dirname, '.env')
 } );
 
